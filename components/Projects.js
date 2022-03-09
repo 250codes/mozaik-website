af907caf539a2976674a1d1435c2ac12851ff582
nkusikevin <nkusikvnhart@gmail.com>
2022-03-09 10:02:31 +0200

    adding testimonials and projects

---
+++ components/Projects.js
@@ -0,0 +1,137 @@
+import React from 'react'
+
+function Projects() {
+  return (
+		<section className='services-area bg-f2f6f9 ptb-110'>
+			<div className='container'>
+				<div className='section-title'>
+					<h2>We Offer Professional Solutions</h2>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+						eiusmod tempor incididunt ut labore et dolore magna aliqua.
+					</p>
+				</div>
+				<div className='row'>
+					<div className='col-lg-4 col-sm-6'>
+						<div className='single-services-box'>
+							<div className='icon'>
+								<i className='flaticon-income'></i>
+							</div>
+							<h3>
+								<a href='service-details.html'>Data Analysts</a>
+							</h3>
+							<p>
+								It is a long established fact that a reader will be distracted
+								by the readable content of a page when looking at its layout.
+							</p>
+						</div>
+					</div>
+					<div className='col-lg-4 col-sm-6'>
+						<div className='single-services-box'>
+							<div className='icon'>
+								<i className='flaticon-automatic'></i>
+							</div>
+							<h3>
+								<a href='service-details.html'>Automatic Optimization</a>
+							</h3>
+							<p>
+								It is a long established fact that a reader will be distracted
+								by the readable content of a page when looking at its layout.
+							</p>
+						</div>
+					</div>
+					<div className='col-lg-4 col-sm-6'>
+						<div className='single-services-box'>
+							<div className='icon'>
+								<i className='flaticon-locked'></i>
+							</div>
+							<h3>
+								<a href='service-details.html'>Security &amp; Surveillance</a>
+							</h3>
+							<p>
+								It is a long established fact that a reader will be distracted
+								by the readable content of a page when looking at its layout.
+							</p>
+						</div>
+					</div>
+					<div className='col-lg-4 col-sm-6'>
+						<div className='single-services-box'>
+							<div className='icon'>
+								<i className='flaticon-molecular'></i>
+							</div>
+							<h3>
+								<a href='service-details.html'>
+									Healthcare &amp; Manufacturing
+								</a>
+							</h3>
+							<p>
+								It is a long established fact that a reader will be distracted
+								by the readable content of a page when looking at its layout.
+							</p>
+						</div>
+					</div>
+					<div className='col-lg-4 col-sm-6'>
+						<div className='single-services-box'>
+							<div className='icon'>
+								<i className='flaticon-gear'></i>
+							</div>
+							<h3>
+								<a href='service-details.html'>Software Engineers</a>
+							</h3>
+							<p>
+								It is a long established fact that a reader will be distracted
+								by the readable content of a page when looking at its layout.
+							</p>
+						</div>
+					</div>
+					<div className='col-lg-4 col-sm-6'>
+						<div className='single-services-box'>
+							<div className='icon'>
+								<i className='flaticon-ceo'></i>
+							</div>
+							<h3>
+								<a href='service-details.html'>IT Professionals</a>
+							</h3>
+							<p>
+								It is a long established fact that a reader will be distracted
+								by the readable content of a page when looking at its layout.
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className='shape-img2'>
+				<img src='images/shape/shape2.svg' alt='image' />
+			</div>
+			<div className='shape-img3'>
+				<img src='images/shape/shape3.png' alt='image' />
+			</div>
+			<div className='shape-img4'>
+				<img src='images/shape/shape4.svg' alt='image' />
+			</div>
+			<div className='shape-img5'>
+				<img src='images/shape/shape5.svg' alt='image' />
+			</div>
+			<div className='shape-img3'>
+				<img src='images/shape/shape3.png' alt='image' />
+			</div>
+			<div className='dot-shape1'>
+				<img src='images/shape/dot1.png' alt='image' />
+			</div>
+			<div className='dot-shape2'>
+				<img src='images/shape/dot3.png' alt='image' />
+			</div>
+			<div className='dot-shape2'>
+				<img src='images/shape/dot4.png' alt='image' />
+			</div>
+			<div className='dot-shape2'>
+				<img src='images/shape/dot5.png' alt='image' />
+			</div>
+			<div className='dot-shape2'>
+				<img src='images/shape/dot6.png' alt='image' />
+			</div>
+		</section>
+	);
+}
+
+export default Projects
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,15 @@
 import Head from "../components/Head";
 import Image from "next/image";
 import HeroSection from "../components/HeroSection";
+import Projects from "../components/Projects";
+import Testimonial from "../components/Testimonial";
 export default function Home() {
 	return (
 		<div>
 			<Head title='MOZAIK' />
 			<HeroSection />
+			<Testimonial />
+			<Projects />
 		</div>
 	);
 }
