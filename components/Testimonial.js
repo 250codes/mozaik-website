4f1d5097b9ce0bfb1801727d6cd8d0cdd7b8615f
ArseneMerci <harsenemerci@gmail.com>
2022-03-15 09:49:38 +0200

    testimonials

--- components/Testimonial.js
+++ components/Testimonial.js
@@ -1,127 +1,45 @@
 import React from 'react'
+import Script from 'next/script'
 
 function Testimonial() {
   return (
-		<section id='testimonials' className='testimonials'>
-			<div className='container'>
-				<div className='section-title' data-aos='fade-up'>
-					<h2>Testimonials</h2>
-					<p>
-						Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex
-						aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos
-						quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia
-						fugiat sit in iste officiis commodi quidem hic quas.
-					</p>
-				</div>
-
-				<div
-					className='testimonials-slider swiper'
-					data-aos='fade-up'
-					data-aos-delay='100'>
-					<div className='swiper-wrapper'>
-						<div className='swiper-slide'>
-							<div className='testimonial-item'>
-								<p>
-									<i className='bx bxs-quote-alt-left quote-icon-left'></i>
-									Proin iaculis purus consequat sem cure digni ssim donec
-									porttitora entum suscipit rhoncus. Accusantium quam, ultricies
-									eget id, aliquam eget nibh et. Maecen aliquam, risus at
-									semper.
-									<i className='bx bxs-quote-alt-right quote-icon-right'></i>
-								</p>
-								<img
-									src='https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
-									className='testimonial-img'
-									alt=''
-								/>
-								<h3>Saul Goodman</h3>
-								<h4>Ceo &amp; Founder</h4>
-							</div>
-						</div>
-
-						<div className='swiper-slide'>
-							<div className='testimonial-item'>
-								<p>
-									<i className='bx bxs-quote-alt-left quote-icon-left'></i>
-									Export tempor illum tamen malis malis eram quae irure esse
-									labore quem cillum quid cillum eram malis quorum velit fore
-									eram velit sunt aliqua noster fugiat irure amet legam anim
-									culpa.
-									<i className='bx bxs-quote-alt-right quote-icon-right'></i>
-								</p>
-								<img
-									src='https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
-									className='testimonial-img'
-									alt=''
-								/>
-								<h3>Sara Wilsson</h3>
-								<h4>Designer</h4>
-							</div>
-						</div>
-
-						<div className='swiper-slide'>
-							<div className='testimonial-item'>
-								<p>
-									<i className='bx bxs-quote-alt-left quote-icon-left'></i>
-									Enim nisi quem export duis labore cillum quae magna enim sint
-									quorum nulla quem veniam duis minim tempor labore quem eram
-									duis noster aute amet eram fore quis sint minim.
-									<i className='bx bxs-quote-alt-right quote-icon-right'></i>
-								</p>
-								<img
-									src='assets/img/testimonials/testimonials-3.jpg'
-									className='testimonial-img'
-									alt=''
-								/>
-								<h3>Jena Karlis</h3>
-								<h4>Store Owner</h4>
-							</div>
-						</div>
-
-						<div className='swiper-slide'>
-							<div className='testimonial-item'>
-								<p>
-									<i className='bx bxs-quote-alt-left quote-icon-left'></i>
-									Fugiat enim eram quae cillum dolore dolor amet nulla culpa
-									multos export minim fugiat minim velit minim dolor enim duis
-									veniam ipsum anim magna sunt elit fore quem dolore labore
-									illum veniam.
-									<i className='bx bxs-quote-alt-right quote-icon-right'></i>
-								</p>
-								<img
-									src='assets/img/testimonials/testimonials-4.jpg'
-									className='testimonial-img'
-									alt=''
-								/>
-								<h3>Matt Brandon</h3>
-								<h4>Freelancer</h4>
-							</div>
-						</div>
-
-						<div className='swiper-slide'>
-							<div className='testimonial-item'>
-								<p>
-									<i className='bx bxs-quote-alt-left quote-icon-left'></i>
-									Quis quorum aliqua sint quem legam fore sunt eram irure aliqua
-									veniam tempor noster veniam enim culpa labore duis sunt culpa
-									nulla illum cillum fugiat legam esse veniam culpa fore nisi
-									cillum quid.
-									<i className='bx bxs-quote-alt-right quote-icon-right'></i>
-								</p>
-								<img
-									src='assets/img/testimonials/testimonials-5.jpg'
-									className='testimonial-img'
-									alt=''
-								/>
-								<h3>John Larson</h3>
-								<h4>Entrepreneur</h4>
-							</div>
-						</div>
-					</div>
-					<div className='swiper-pagination'></div>
-				</div>
-			</div>
-		</section>
+<div className="testimonials-clean">
+        <div className="container">
+            <div className="intro">
+                <h2 className="text-center">Testimonials </h2>
+                {/* <p className="text-center">Our customers love us! Read what they have to say below. Aliquam sed justo ligula. Vestibulum nibh erat, pellentesque ut laoreet vitae.</p> */}
+            </div>
+            <div className="row people">
+                <div className="col-md-6 col-lg-4 item">
+                    <div className="box">
+                        <p className="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est.</p>
+                    </div>
+                    <div className="author"><img className="rounded-circle" src="../images/page-banner-bg.jpg" />
+                        <h5 className="name">John Doe</h5>
+                        <p className="title">CEO of Company Inc.</p>
+                    </div>
+                </div>
+                <div className="col-md-6 col-lg-4 item">
+                    <div className="box">
+                        <p className="description">Praesent aliquam in tellus eu gravida. Aliquam varius finibus est, et interdum justo suscipit id.</p>
+                    </div>
+                    <div className="author"><img className="rounded-circle" src="/page-banner-bg.jpg"/>
+                        <h5 className="name">John Doe</h5>
+                        <p className="title">Founder of Style Co.</p>
+                    </div>
+                </div>
+                <div className="col-md-6 col-lg-4 item">
+                    <div className="box">
+                        <p className="description">Aliquam varius finibus est, et interdum justo suscipit. Vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu.</p>
+                    </div>
+                    <div className="author"><img className="rounded-circle" src="/testimonials-1.jpg"/>
+                        <h5 className="name">John Doe</h5>
+                        <p className="title">Owner of Creative Ltd.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 	);
 }
 
--- pages/_app.js
+++ pages/_app.js
@@ -4,24 +4,29 @@ import "bootstrap/dist/css/bootstrap.css";
 
 import Layout from "../components/Layout";
 import Script from 'next/script'
-import "../styles/css/animate.css";
+// import "../styles/css/animate.css";
 // import "../styles/aos.css"
 import "../styles/css/bootstrap.css";
 import "../styles/css/bootstrap-icons/bootstrap-icons.css";
+// import "../styles/vendor/bootstrap/css/bootstrap.min.css";
 import "../styles/globals.css"
 import "../styles/about.css"
-import "../styles/swiper-bundle.min.css";
-import "../styles/vendor/boxicons/css/boxicons.css"
+import "../styles/vendor/swiper/swiper-bundle.min.css";
+// import "../styles/vendor/boxicons/css/boxicons.css"
 import "../styles/testimonial.css"
 
 function MyApp({ Component, pageProps }) {
-	useEffect(() => {
-		import("bootstrap/dist/js/bootstrap.bundle.min.js");
-	}, []);
+	// useEffect(() => {
+	// 	import("bootstrap/dist/js/bootstrap.bundle.min.js");
+	// }, []);
 	return (
 		<Layout>
+			<Script src="../js/testimonial.js" />
+			<Script src='https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha1/dist/js/bootstrap.bundle.min.js'/>
+			<Script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js'/>
+			<Script src="../styles/vendor/swiper/swiper-bundle.min.js" />
 			<Script src="/js/bootstrap.bundle.min.js" />
-			<Script src="../js/main.js" />
+			{/* <Script src="../js/main.js" /> */}
 			<Component {...pageProps} />
 		</Layout>
 	);
