afa852800e610b1d06440bc0deeddc5dc3a0f881
nkusikevin <nkusikvnhart@gmail.com>
2022-03-18 18:30:49 +0200

    adding meta tags for SEO

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,19 @@ export default function Document() {
 		<Html>
 			<Head>
 				<link rel='shortcut icon' href='/images/logo.png' />
+				<meta name='title' content='Moziak concepts' />
+				<meta
+					name='description'
+					content='This is company that is based in Kigali-Rwanda that deals with interior design and construction'
+				/>
+				<meta
+					name='keywords'
+					content='interior design , building , construction'
+				/>
+				<meta name='robots' content='index, follow' />
+				<meta content='text/html; charset=utf-8' />
+				<meta name='language' content='English' />
+				<meta name='author' content='moziak concepts' />
 			</Head>
 			<body>
 				<Main />
