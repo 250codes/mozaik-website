9b89d40409826233b495012a1293d2b5dd2c5d8c
nkusikevin <nkusikvnhart@gmail.com>
2022-05-19 22:02:52 +0200

    adding link redirect

--- components/ServicesPagesBody.js
+++ components/ServicesPagesBody.js
@@ -1,26 +1,27 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 function ServicesPagesBody({ image, text }) {
 	return (
-        <>
-					<div className='col-md-8'>
-						<Image
-							className='img-fluid w-100'
-							layout='intrinsic'
-							width={700}
-							height={475}
-							src={ image }
-							alt=''
-						/>
-					</div>
-					<div className='col-md-4'>
-						<p className="text-justify">
-							{ text }
-						</p>
-						<button className='btn contactUs-btn-bg mt-3'>Contact Us</button>
-					</div>
-        </>
+		<>
+			<div className='col-md-8'>
+				<Image
+					className='img-fluid w-100'
+					layout='intrinsic'
+					width={700}
+					height={475}
+					src={image}
+					alt=''
+				/>
+			</div>
+			<div className='col-md-4'>
+				<p className='text-justify'>{text}</p>
+				<div className='btn contactUs-btn-bg mt-3'>
+					<Link href='Contact'>Contact Us</Link>
+				</div>
+			</div>
+		</>
 	);
 }
 
